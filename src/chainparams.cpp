36f8973f90832afc2247eec28d0d27715ef63e8b
Benjamin Allred <nihilian@live.com>
2018-08-09 18:25:37 -0600

    Ported CLTV from BIP65 to OBSR. Added checks for supermajority activation.
    Signed-off-by: observerdev <dev@obsr.org>

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -140,9 +140,9 @@ public:
         bnProofOfWorkLimit = ~uint256(0) >> 20; // OBSR starting difficulty is 1 / 2^12
         nSubsidyHalvingInterval = 210000;
         nMaxReorganizationDepth = 100;
-        nEnforceBlockUpgradeMajority = 750;
-        nRejectBlockOutdatedMajority = 950;
-        nToCheckBlockUpgradeMajority = 1000;
+        nEnforceBlockUpgradeMajority = 1080;
+        nRejectBlockOutdatedMajority = 1368;
+        nToCheckBlockUpgradeMajority = 1440;
         nMinerThreads = 0;
         nTargetTimespan = 1 * 60; // OBSR: 1 day
         nTargetSpacing = 1 * 60;  // OBSR: 1 minute
