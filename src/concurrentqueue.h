furszy/bitcoin-core

Zerocoin Light Node Protocol

src/concurrentqueue.h
@@ -1,0 +1,50 @@
+//
+// Copyright (c) 2015-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PIVX_CONCURRENTQUEUE_H
+#define PIVX_CONCURRENTQUEUE_H
+
+
+#include <mutex>
+#include <condition_variable>
+#include <deque>
+
+template <typename T>
+class concurrentqueue
+{
+private:
+    std::mutex              mutex;
+    std::condition_variable condition;
+    std::deque<T>           queue;
+public:
+    void push(T const& value) {
+        {
+            std::unique_lock<std::mutex> lock(this->mutex);
+            queue.push_front(value);
+        }
+        this->condition.notify_one();
+    }
+    T pop() {
+        std::unique_lock<std::mutex> lock(this->mutex);
+        this->condition.wait(lock, [=]{ return !this->queue.empty(); });
+        T rc(std::move(this->queue.back()));
+        this->queue.pop_back();
+        return rc;
+    }
+
+    T popNotWait(){
+        std::unique_lock<std::mutex> lock(this->mutex);
+        T rc(std::move(this->queue.back()));
+        this->queue.pop_back();
+        return rc;
+    }
+
+    bool hasElements(){
+        std::unique_lock<std::mutex> lock(this->mutex);
+        return !queue.empty();
+    }
+};
+
+#endif //PIVX_CONCURRENTQUEUE_H

src/genwit.h
@@ -1,0 +1,66 @@
+// Copyright (c) 2015-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PIVX_GENWIT_H
+#define PIVX_GENWIT_H
+
+
+#include <iostream>
+#include "bloom.h"
+#include "libzerocoin/Denominations.h"
+#include "net.h"
+
+class CGenWit {
+
+    public:
+
+    CGenWit();
+
+    CGenWit(const CBloomFilter &filter, int startingHeight, libzerocoin::CoinDenomination den, int requestNum, CBigNum accWitValue = 0);
+
+    bool isValid(int chainActiveHeight);
+
+    ADD_SERIALIZE_METHODS;
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(filter);
+        filter.setFull();
+        READWRITE(startingHeight);
+        READWRITE(den);
+        READWRITE(requestNum);
+        try {
+            // TODO: This is for the old testnet nodes that are running my code..
+            READWRITE(accWitValue);
+        }catch (std::exception& e){
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    const CBloomFilter &getFilter() const;
+
+    int getStartingHeight() const;
+
+    libzerocoin::CoinDenomination getDen() const;
+
+    int getRequestNum() const;
+
+    CNode *getPfrom() const;
+
+    void setPfrom(CNode *pfrom);
+
+    const CBigNum &getAccWitValue() const;
+
+    const std::string toString() const;
+
+private:
+    CBloomFilter filter;
+    int startingHeight;
+    libzerocoin::CoinDenomination den;
+    int requestNum;
+    CBigNum accWitValue;
+    CNode* pfrom;
+};
+
+
+#endif //PIVX_GENWIT_H

src/init.cpp
@@ -284,6 +284,10 @@ void Shutdown()
     }
     // Shutdown part 2: Stop TOR thread and delete wallet instance
     StopTorControl();
+    // Shutdown witness thread if it's enabled
+    if (nLocalServices == NODE_BLOOM_LIGHT_ZC) {
+        lightWorker.StopLightZpivThread();
+    }
 #ifdef ENABLE_WALLET
     delete pwalletMain;
     pwalletMain = NULL;
@@ -410,6 +414,7 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += HelpMessageOpt("-onlynet=<net>", _("Only connect to nodes in network <net> (ipv4, ipv6 or onion)"));
     strUsage += HelpMessageOpt("-permitbaremultisig", strprintf(_("Relay non-P2SH multisig (default: %u)"), 1));
     strUsage += HelpMessageOpt("-peerbloomfilters", strprintf(_("Support filtering of blocks and transaction with bloom filters (default: %u)"), DEFAULT_PEERBLOOMFILTERS));
+    strUsage += HelpMessageOpt("-peerbloomfilterszc", strprintf(_("Support the zerocoin light node protocol (default: %u)"), DEFAULT_PEERBLOOMFILTERS_ZC));
     strUsage += HelpMessageOpt("-port=<port>", strprintf(_("Listen for connections on <port> (default: %u or testnet: %u)"), 51472, 51474));
     strUsage += HelpMessageOpt("-proxy=<ip:port>", _("Connect through SOCKS5 proxy"));
     strUsage += HelpMessageOpt("-proxyrandomize", strprintf(_("Randomize credentials for every proxy connection. This enables Tor stream isolation (default: %u)"), 1));
@@ -981,9 +986,15 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
 
     fAlerts = GetBoolArg("-alerts", DEFAULT_ALERTS);
 
+    if (GetBoolArg("-peerbloomfilterszc", DEFAULT_PEERBLOOMFILTERS_ZC))
+        nLocalServices |= NODE_BLOOM_LIGHT_ZC;
 
-    if (GetBoolArg("-peerbloomfilters", DEFAULT_PEERBLOOMFILTERS))
-        nLocalServices |= NODE_BLOOM;
+    if (nLocalServices != NODE_BLOOM_LIGHT_ZC) {
+
+        if (GetBoolArg("-peerbloomfilters", DEFAULT_PEERBLOOMFILTERS))
+            nLocalServices |= NODE_BLOOM;
+
+    }
 
     // ********************************************************* Step 4: application initialization: dir lock, daemonize, pidfile, debug log
 
@@ -1915,6 +1926,11 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
 
     StartNode(threadGroup, scheduler);
 
+    if (nLocalServices & NODE_BLOOM_LIGHT_ZC) {
+        // Run a thread to compute witnesses
+        lightWorker.StartLightZpivThread(threadGroup);
+    }
+
 #ifdef ENABLE_WALLET
     // Generate coins in the background
     if (pwalletMain)
@@ -1935,6 +1951,7 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
         threadGroup.create_thread(boost::bind(&ThreadFlushWalletDB, boost::ref(pwalletMain->strWalletFile)));
     }
 #endif
+
 
     return !fRequestShutdown;
 }

src/rpc/blockchain.cpp
@@ -16,9 +16,13 @@
 #include "utilmoneystr.h"
 #include "accumulatormap.h"
 #include "accumulators.h"
-
+#include "wallet.h"
+#include "libzerocoin/Coin.h"
 #include <stdint.h>
+#include <fstream>
+#include <iostream>
 #include <univalue.h>
+#include "libzerocoin/bignum.h"
 
 using namespace std;
 
@@ -1013,4 +1017,91 @@ UniValue getaccumulatorvalues(const UniValue& params, bool fHelp)
     }
 
     return ret;
+}
+
+/**
+ * TODO: Clean me please.. validate inputs..
+ *
+ */
+UniValue getaccumulatorwitness(const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() != 2)
+        throw runtime_error(
+                "getaccumulatorwitness \"commitmentCoinValue, coinDenomination\"\n"
+                "\nReturns the accumulator witness value associated with the coin\n"
+
+                "\nArguments:\n"
+                "1. commitmentCoinValue   (numeric, required) the commitment value of the coin in DEC.\n"
+                "2. coinDenomination   (numeric, required) the coin denomination.\n"
+
+                "\nExamples:\n" +
+                HelpExampleCli("getaccumulatorvalues", "\"height\"") + HelpExampleRpc("getaccumulatorvalues", "\"height\""));
+
+
+    CBigNum coinCommitmentValue;
+    coinCommitmentValue.SetHex(params[0].get_str());
+
+    int d = std::stoi(params[1].get_str());
+    libzerocoin::CoinDenomination denomination = libzerocoin::IntToZerocoinDenomination(d);
+    libzerocoin::ZerocoinParams* paramsAccumulator = Params().Zerocoin_Params(false);
+
+    // Public coin
+    libzerocoin::PublicCoin pubCoinSelected(paramsAccumulator, coinCommitmentValue, denomination);
+
+    //Compute Accumulator and Witness
+    libzerocoin::Accumulator accumulator(paramsAccumulator, pubCoinSelected.getDenomination());
+    libzerocoin::AccumulatorWitness witness(paramsAccumulator, accumulator, pubCoinSelected);
+    string strFailReason = "";
+    int nMintsAdded = 0;
+    CZerocoinSpendReceipt receipt;
+    if (!GenerateAccumulatorWitness(pubCoinSelected, accumulator, witness, 100, nMintsAdded, strFailReason)) {
+        receipt.SetStatus(_("Try to spend with a higher security level to include more coins"), ZPIV_FAILED_ACCUMULATOR_INITIALIZATION);
+        throw JSONRPCError(RPC_DATABASE_ERROR, receipt.GetStatusMessage());
+    }
+
+    UniValue ret(UniValue::VARR);
+
+    UniValue obj(UniValue::VOBJ);
+    obj.push_back(Pair("value", accumulator.getValue().GetHex()));
+    obj.push_back(Pair("denomination", accumulator.getDenomination()));
+    obj.push_back(Pair("Mints added",nMintsAdded));
+    ret.push_back(obj);
+
+    UniValue obj1(UniValue::VOBJ);
+    obj1.push_back(Pair("witness value", witness.getValue().GetHex()));
+    obj1.push_back(Pair("PublicCoin value", witness.getPublicCoin().getValue().GetHex()));
+    obj1.push_back(Pair("PublicCoin denomination", witness.getPublicCoin().getDenomination()));
+    ret.push_back(obj1);
+
+    return ret;
+}
+
+UniValue getmintsvalues(const UniValue& params, bool fHelp) {
+    if (fHelp || params.size() != 3)
+        throw runtime_error(
+                "getmintsvalues \"height, coinDenomination\"\n"
+                "\nReturns the mints that occurred on a certain block and denomination\n"
+
+                "\nArguments:\n"
+                "1. height   (numeric, required) the block height.\n"
+                "2. coinDenomination   (numeric, required) the coin denomination.\n"
+
+                "\nExamples:\n" +
+                HelpExampleCli("getmintsvalues", "\"height\"") +
+                HelpExampleRpc("getmintsvalues", "\"height\""));
+
+
+
+    int heightStart = std::stoi(params[0].get_str());
+    int heightEnd = std::stoi(params[1].get_str());
+    int d = params[2].get_int();
+    libzerocoin::CoinDenomination den = libzerocoin::IntToZerocoinDenomination(d);
+
+    CBlockIndex* blockIndexStart = chainActive[heightStart];
+    CBlockIndex* blockIndexEnd = chainActive[heightEnd];
+
+    UniValue obj(UniValue::VOBJ);
+    obj.push_back(Pair("value", std::to_string(blockIndexEnd->mapZerocoinSupply.at(den) - blockIndexStart->mapZerocoinSupply.at(den)) ));
+
+    return obj;
 }

observernet/obsr

Signed-off-by: observerdev <dev@obsr.org>

src/concurrentqueue.h
@@ -1,0 +1,50 @@
+//
+// Copyright (c) 2015-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef OBSR_CONCURRENTQUEUE_H
+#define OBSR_CONCURRENTQUEUE_H
+
+
+#include <mutex>
+#include <condition_variable>
+#include <deque>
+
+template <typename T>
+class concurrentqueue
+{
+private:
+    std::mutex              mutex;
+    std::condition_variable condition;
+    std::deque<T>           queue;
+public:
+    void push(T const& value) {
+        {
+            std::unique_lock<std::mutex> lock(this->mutex);
+            queue.push_front(value);
+        }
+        this->condition.notify_one();
+    }
+    T pop() {
+        std::unique_lock<std::mutex> lock(this->mutex);
+        this->condition.wait(lock, [=]{ return !this->queue.empty(); });
+        T rc(std::move(this->queue.back()));
+        this->queue.pop_back();
+        return rc;
+    }
+
+    T popNotWait(){
+        std::unique_lock<std::mutex> lock(this->mutex);
+        T rc(std::move(this->queue.back()));
+        this->queue.pop_back();
+        return rc;
+    }
+
+    bool hasElements(){
+        std::unique_lock<std::mutex> lock(this->mutex);
+        return !queue.empty();
+    }
+};
+
+#endif //OBSR_CONCURRENTQUEUE_H

src/genwit.h
@@ -1,0 +1,66 @@
+// Copyright (c) 2015-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef OBSR_GENWIT_H
+#define OBSR_GENWIT_H
+
+
+#include <iostream>
+#include "bloom.h"
+#include "libzerocoin/Denominations.h"
+#include "net.h"
+
+class CGenWit {
+
+    public:
+
+    CGenWit();
+
+    CGenWit(const CBloomFilter &filter, int startingHeight, libzerocoin::CoinDenomination den, int requestNum, CBigNum accWitValue = 0);
+
+    bool isValid(int chainActiveHeight);
+
+    ADD_SERIALIZE_METHODS;
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(filter);
+        filter.setFull();
+        READWRITE(startingHeight);
+        READWRITE(den);
+        READWRITE(requestNum);
+        try {
+            // TODO: This is for the old testnet nodes that are running my code..
+            READWRITE(accWitValue);
+        }catch (std::exception& e){
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    const CBloomFilter &getFilter() const;
+
+    int getStartingHeight() const;
+
+    libzerocoin::CoinDenomination getDen() const;
+
+    int getRequestNum() const;
+
+    CNode *getPfrom() const;
+
+    void setPfrom(CNode *pfrom);
+
+    const CBigNum &getAccWitValue() const;
+
+    const std::string toString() const;
+
+private:
+    CBloomFilter filter;
+    int startingHeight;
+    libzerocoin::CoinDenomination den;
+    int requestNum;
+    CBigNum accWitValue;
+    CNode* pfrom;
+};
+
+
+#endif //OBSR_GENWIT_H

src/init.cpp
@@ -291,6 +291,10 @@ void Shutdown()
     }
     // Shutdown part 2: Stop TOR thread and delete wallet instance
     StopTorControl();
+    // Shutdown witness thread if it's enabled
+    if (nLocalServices == NODE_BLOOM_LIGHT_ZC) {
+        lightWorker.StopLightZobsrThread();
+    }
 #ifdef ENABLE_WALLET
     delete pwalletMain;
     pwalletMain = NULL;
@@ -417,6 +421,7 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += HelpMessageOpt("-onlynet=<net>", _("Only connect to nodes in network <net> (ipv4, ipv6 or onion)"));
     strUsage += HelpMessageOpt("-permitbaremultisig", strprintf(_("Relay non-P2SH multisig (default: %u)"), 1));
     strUsage += HelpMessageOpt("-peerbloomfilters", strprintf(_("Support filtering of blocks and transaction with bloom filters (default: %u)"), DEFAULT_PEERBLOOMFILTERS));
+    strUsage += HelpMessageOpt("-peerbloomfilterszc", strprintf(_("Support the zerocoin light node protocol (default: %u)"), DEFAULT_PEERBLOOMFILTERS_ZC));
     strUsage += HelpMessageOpt("-port=<port>", strprintf(_("Listen for connections on <port> (default: %u or testnet: %u)"), 9567, 29567));
     strUsage += HelpMessageOpt("-proxy=<ip:port>", _("Connect through SOCKS5 proxy"));
     strUsage += HelpMessageOpt("-proxyrandomize", strprintf(_("Randomize credentials for every proxy connection. This enables Tor stream isolation (default: %u)"), 1));
@@ -992,9 +997,15 @@ bool AppInit2()
 
     fAlerts = GetBoolArg("-alerts", DEFAULT_ALERTS);
 
+    if (GetBoolArg("-peerbloomfilterszc", DEFAULT_PEERBLOOMFILTERS_ZC))
+        nLocalServices |= NODE_BLOOM_LIGHT_ZC;
 
-    if (GetBoolArg("-peerbloomfilters", DEFAULT_PEERBLOOMFILTERS))
-        nLocalServices |= NODE_BLOOM;
+    if (nLocalServices != NODE_BLOOM_LIGHT_ZC) {
+
+        if (GetBoolArg("-peerbloomfilters", DEFAULT_PEERBLOOMFILTERS))
+            nLocalServices |= NODE_BLOOM;
+
+    }
 
     // ********************************************************* Step 4: application initialization: dir lock, daemonize, pidfile, debug log
 
@@ -1928,6 +1939,11 @@ bool AppInit2()
 
     StartNode(threadGroup, scheduler);
 
+    if (nLocalServices & NODE_BLOOM_LIGHT_ZC) {
+        // Run a thread to compute witnesses
+        lightWorker.StartLightZobsrThread(threadGroup);
+    }
+
 #ifdef ENABLE_WALLET
     // Generate coins in the background
     if (pwalletMain)
@@ -1948,6 +1964,7 @@ bool AppInit2()
         threadGroup.create_thread(boost::bind(&ThreadFlushWalletDB, boost::ref(pwalletMain->strWalletFile)));
     }
 #endif
+
 
     return !fRequestShutdown;
 }

src/rpc/blockchain.cpp
@@ -17,9 +17,13 @@
 #include "utilmoneystr.h"
 #include "accumulatormap.h"
 #include "accumulators.h"
-
+#include "wallet.h"
+#include "libzerocoin/Coin.h"
 #include <stdint.h>
+#include <fstream>
+#include <iostream>
 #include <univalue.h>
+#include "libzerocoin/bignum.h"
 
 using namespace std;
 
@@ -1014,4 +1018,91 @@ UniValue getaccumulatorvalues(const UniValue& params, bool fHelp)
     }
 
     return ret;
+}
+
+/**
+ * TODO: Clean me please.. validate inputs..
+ *
+ */
+UniValue getaccumulatorwitness(const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() != 2)
+        throw runtime_error(
+                "getaccumulatorwitness \"commitmentCoinValue, coinDenomination\"\n"
+                "\nReturns the accumulator witness value associated with the coin\n"
+
+                "\nArguments:\n"
+                "1. commitmentCoinValue   (numeric, required) the commitment value of the coin in DEC.\n"
+                "2. coinDenomination   (numeric, required) the coin denomination.\n"
+
+                "\nExamples:\n" +
+                HelpExampleCli("getaccumulatorvalues", "\"height\"") + HelpExampleRpc("getaccumulatorvalues", "\"height\""));
+
+
+    CBigNum coinCommitmentValue;
+    coinCommitmentValue.SetHex(params[0].get_str());
+
+    int d = std::stoi(params[1].get_str());
+    libzerocoin::CoinDenomination denomination = libzerocoin::IntToZerocoinDenomination(d);
+    libzerocoin::ZerocoinParams* paramsAccumulator = Params().Zerocoin_Params(false);
+
+    // Public coin
+    libzerocoin::PublicCoin pubCoinSelected(paramsAccumulator, coinCommitmentValue, denomination);
+
+    //Compute Accumulator and Witness
+    libzerocoin::Accumulator accumulator(paramsAccumulator, pubCoinSelected.getDenomination());
+    libzerocoin::AccumulatorWitness witness(paramsAccumulator, accumulator, pubCoinSelected);
+    string strFailReason = "";
+    int nMintsAdded = 0;
+    CZerocoinSpendReceipt receipt;
+    if (!GenerateAccumulatorWitness(pubCoinSelected, accumulator, witness, 100, nMintsAdded, strFailReason)) {
+        receipt.SetStatus(_("Try to spend with a higher security level to include more coins"), ZOBSR_FAILED_ACCUMULATOR_INITIALIZATION);
+        throw JSONRPCError(RPC_DATABASE_ERROR, receipt.GetStatusMessage());
+    }
+
+    UniValue ret(UniValue::VARR);
+
+    UniValue obj(UniValue::VOBJ);
+    obj.push_back(Pair("value", accumulator.getValue().GetHex()));
+    obj.push_back(Pair("denomination", accumulator.getDenomination()));
+    obj.push_back(Pair("Mints added",nMintsAdded));
+    ret.push_back(obj);
+
+    UniValue obj1(UniValue::VOBJ);
+    obj1.push_back(Pair("witness value", witness.getValue().GetHex()));
+    obj1.push_back(Pair("PublicCoin value", witness.getPublicCoin().getValue().GetHex()));
+    obj1.push_back(Pair("PublicCoin denomination", witness.getPublicCoin().getDenomination()));
+    ret.push_back(obj1);
+
+    return ret;
+}
+
+UniValue getmintsvalues(const UniValue& params, bool fHelp) {
+    if (fHelp || params.size() != 3)
+        throw runtime_error(
+                "getmintsvalues \"height, coinDenomination\"\n"
+                "\nReturns the mints that occurred on a certain block and denomination\n"
+
+                "\nArguments:\n"
+                "1. height   (numeric, required) the block height.\n"
+                "2. coinDenomination   (numeric, required) the coin denomination.\n"
+
+                "\nExamples:\n" +
+                HelpExampleCli("getmintsvalues", "\"height\"") +
+                HelpExampleRpc("getmintsvalues", "\"height\""));
+
+
+
+    int heightStart = std::stoi(params[0].get_str());
+    int heightEnd = std::stoi(params[1].get_str());
+    int d = params[2].get_int();
+    libzerocoin::CoinDenomination den = libzerocoin::IntToZerocoinDenomination(d);
+
+    CBlockIndex* blockIndexStart = chainActive[heightStart];
+    CBlockIndex* blockIndexEnd = chainActive[heightEnd];
+
+    UniValue obj(UniValue::VOBJ);
+    obj.push_back(Pair("value", std::to_string(blockIndexEnd->mapZerocoinSupply.at(den) - blockIndexStart->mapZerocoinSupply.at(den)) ));
+
+    return obj;
 }