d0de5884c104e468ea95cfe18e0f0134ae427202
observerdev <dev@obsr.org>
2019-04-09 15:56:42 +0200

    release v1.1.00
    - reverted snap to latest stable working
    - use make -j4 to speed up compilation
    - remove GLIBC change for s390x and now version is printed in logs if changed

--- src/compat/glibc_compat.cpp
+++ src/compat/glibc_compat.cpp
@@ -69,8 +69,6 @@ __asm(".symver log2f_old,log2f@GLIBC_2.2.5");
 __asm(".symver log2f_old,log2f@GLIBC_2.4");
 #elif defined(__aarch64__)
 __asm(".symver log2f_old,log2f@GLIBC_2.17");
-#elif defined(__s390x__)
-__asm(".symver log2f_old,log2f@GLIBC_2.17");
 #elif defined(__riscv)
 __asm(".symver log2f_old,log2f@GLIBC_2.27");
 #endif
